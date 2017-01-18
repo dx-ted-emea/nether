c22f5cdb1c9a8698690921535d20414bdcd642ad
Kristofer Liljeblad <kristol@microsoft.com>
2017-01-18 17:11:10 +0100

    First version of working WebJob using WebJobSDK for EventHub

--- src/Nether.Analytics.EventProcessor/Program.cs
+++ src/Nether.Analytics.EventProcessor/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +9,34 @@ using System.Threading.Tasks;
 
 namespace Nether.Analytics.EventProcessor
 {
-    class Program
+    public class Program
     {
+        private const string InputEventHubName = "analytics";
+        private const string IntermediateEventHubName = "nether-intermediate";
+
         static void Main(string[] args)
         {
+            // Endpoint = sb://nether.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h16jv6nc0bVfgdWrZp7f5Fpau4jaSu2YH+U2xg0YI14=
+
+
+            var eventHubConfig = new EventHubConfiguration();
+            eventHubConfig.AddReceiver(InputEventHubName, "Endpoint=sb://nether.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h16jv6nc0bVfgdWrZp7f5Fpau4jaSu2YH+U2xg0YI14=");
+            //eventHubConfig.AddSender(IntermediateEventHubName, "Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=SendRule;SharedAccessKey=xxxxxxxx");
+
+
+            var webJobDashboardConnectionString = "DefaultEndpointsProtocol=https;AccountName=netherdashboard;AccountKey=oT30a8/BSwTFg/4GGWLPCeGIHBfgDcMf9zEThKHlY4hjUNy3sYUTSWXWa3yJMoX2lvTnWSIrjtwU9kg9YaL0Qw==";
+            var jobHostConfig = new JobHostConfiguration(webJobDashboardConnectionString);
+            jobHostConfig.UseEventHub(eventHubConfig);
+
+            var host = new JobHost(jobHostConfig);
+            host.RunAndBlock();
         }
+
+        public static void HandleOne([EventHubTrigger(InputEventHubName)] string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        //static void SendOne([EventHub("MyHub")] out EventData output);
     }
 }
