682a288021a7feb69d1395fd2e7e40b3925e91b6
Kristofer Liljeblad <kristol@microsoft.com>
2017-01-19 11:44:00 +0100

    Cleand up test project and added file to hold test for Appender

---
+++ tests/Nether.Analytics.EventProcessor.Tests/CsvBlobAppenderTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Nether.Analytics.EventProcessor.Tests
+{
+    public class CsvBlobAppenderTests
+    {
+        [Fact]
+        void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
