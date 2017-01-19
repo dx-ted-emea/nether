8b68941060e9adcc5864de1bfbfe39d583e11d4d
Andreas Pohl <Annonator@googlemail.com>
2017-01-19 09:56:59 +0100

    Added Stop Event and Fixed Error in Start Event

--- src/Nether.Analytics.EventProcessor/Models/StopEvent.cs
+++ src/Nether.Analytics.EventProcessor/Models/StopEvent.cs
@@ -1,11 +1,10 @@
 namespace Nether.Analytics.EventProcessor.Models
 {
-    public class StartEvent : GameEvent
+    public class StopEvent : GameEvent
     {
         public override string Event => "start";
         public override string Version => "1.0.0";
         public string EventCorrelationId { get; set; }
-        public string DurationName { get; set; }
         public string GameSessionId { get; set; }
     }
 }
