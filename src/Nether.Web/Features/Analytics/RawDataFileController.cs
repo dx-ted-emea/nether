db134145c3b2ddb14c8b5b03a15068b81fd7d892
Vito Flavio Lorusso <vf.lorusso@live.com>
2017-02-07 09:09:48 +0100

    data structure for rwafiledata get and web api stub

---
+++ src/Nether.Web/Features/Analytics/RawDataFileController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.WindowsAzure.Storage.Core;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+using Nether.Data.Analytics;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Nether.Web.Features.Analytics
+{
+    // TODO: get from configuration
+    string storageConnectionString = "";
+    [Route("api/[controller]")]
+    public class RawDataFileController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new RawDataFile[] { };
+        }
+
+        // GET api/values/20170120
+        [HttpGet("{dateFilter}")]
+        public string Get(string dateFilter)
+        {
+            return "value";
+        }
+
+    }
+}
