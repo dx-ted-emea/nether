2fb37470d31b0fb1b2a5585e5e78e93d65f85440
Vito Flavio Lorusso <vf.lorusso@live.com>
2017-02-07 09:09:48 +0100

    data structure for rwafiledata get and web api stub

---
+++ src/Nether.Data/Analytics/RawDataFile.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Nether.Data.Analytics
+{
+    [DebuggerDisplay("RawDataFile (name '{Name}')")]
+    public class RawDataFile
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
