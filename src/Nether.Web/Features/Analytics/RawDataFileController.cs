aafaf4386addb5aeea1282afecc27bc96fb63df0
Vito Flavio Lorusso <vf.lorusso@live.com>
2017-02-07 09:49:00 +0100

    api stub with storage account access

--- src/Nether.Web/Features/Analytics/RawDataFileController.cs
+++ src/Nether.Web/Features/Analytics/RawDataFileController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-using Microsoft.WindowsAzure.Storage.Core;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 
 using Nether.Data.Analytics;
@@ -13,24 +14,34 @@ using Nether.Data.Analytics;
 
 namespace Nether.Web.Features.Analytics
 {
-    // TODO: get from configuration
-    string storageConnectionString = "";
     [Route("api/[controller]")]
     public class RawDataFileController : Controller
     {
+        // TODO: get from configuration
+        string storageConnectionString = @"DefaultEndpointsProtocol=http;AccountName=account;AccountKey=key";
+        string containerName = "analyticsdata";
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<RawDataFile> Get()
         {
-            return new RawDataFile[] { };
+            return GetRawDataFile("");
         }
 
         // GET api/values/20170120
         [HttpGet("{dateFilter}")]
-        public string Get(string dateFilter)
+        public IEnumerable<RawDataFile> Get(string dateFilter)
         {
-            return "value";
+            return GetRawDataFile(dateFilter);
         }
 
+        private RawDataFile[] GetRawDataFile(string datefilter)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(containerName);
+            // TODO: create container Sas list blobs and create Sas donwload Url
+
+            return new RawDataFile[] { };
+        }
     }
 }
